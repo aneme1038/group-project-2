cf84c6a94557b3e6a0e82c5457f81471a324d6c8
A Winslow Mays <49768250+winslowdev@users.noreply.github.com>
2019-09-07 11:20:38 -0400

    fixed stuff; broke stuff

--- app/javascript/components/Post.js
+++ app/javascript/components/Post.js
@@ -8,31 +8,32 @@ import React from 'react'
 // COMPONENT CLASS
 // =============================
 class Post extends React.Component {
-  // ==============
-  // RENDER
-  // ==============
-  render () {
-    return (
-      <article>
-        <div className="post-header">
-
-        </div>
-        <div className="post-body">
-          {this.props.postData.body}
-        </div>
-        <div className="post-options">
-          <ul>
-            <li onClick={() => {
-              this.props.handleView('editPost', this.props.postData)
-            }}>edit post</li>
-            <li onClick={() => {
-              this.props.handleDelete(this.props.postData.id)
-            }}>delete post</li>
-          </ul>
-        </div>
-      </article>
-    )
-  }
+    // ==============
+    // RENDER
+    // ==============
+    render() {
+        return (
+            <article>
+                <div className="post-header">
+                    <img src={this.props.postData.avatar} alt="" />
+                    <h1>{this.props.postData.username}</h1>
+                </div>
+                <div className="post-body">
+                    {this.props.postData.body}
+                </div>
+                <div className="post-options">
+                    <ul>
+                        <li onClick={() => {
+                            this.props.handleView('editPost', this.props.postData)
+                        }}>edit post</li>
+                        <li onClick={() => {
+                            this.props.handleDelete(this.props.postData.id)
+                        }}>delete post</li>
+                    </ul>
+                </div>
+            </article>
+        )
+    }
 }
 
 // =============================
