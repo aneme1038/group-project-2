3e351ef5647a1f4a57a0a2b0e1ece371fcb3a8fd
A Winslow Mays <49768250+winslowdev@users.noreply.github.com>
2019-09-09 15:52:50 -0400

    fixer app error

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,13 @@ class Post
   attr_reader :id
 
   # connect to postgres
-  DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'group_project_2_development'})
+  if(ENV['DATABASE_URL'])
+    uri = URI.parse(ENV['DATABASE_URL'])
+    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+else
+    DB = PG.connect(host: "localhost", port: 5432, dbname: 'group_project_2_development')
+end
+#   DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'group_project_2_development'})
 
   # ===============================
   # PREPARED STATEMENTS
